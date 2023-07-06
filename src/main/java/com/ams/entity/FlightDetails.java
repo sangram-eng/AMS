e98b9a66bfdf468be1ff71b648f6e58c153c7885
sangramnay421@gmail.com <sangramnay421@gmail.com>
2023-07-06 22:37:28 +0530

    flightDetails entity class commit

--- src/main/java/com/ams/entity/FlightDetails.java
+++ src/main/java/com/ams/entity/FlightDetails.java
@@ -1,5 +1,24 @@
 package com.ams.entity;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+@Data
+@Entity
+@Table(name="flight_details")
 public class FlightDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String flightName;    
+	@Column
+	private String flightZone;
+	@Column
+	private String	flightCode;
 
 }
