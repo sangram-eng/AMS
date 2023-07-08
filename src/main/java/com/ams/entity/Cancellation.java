6e19679bbca0d8a52c8496d0caeb5817f2381420
rajendrarout <rajendrarout888@gmail.com>
2023-07-08 21:20:36 +0530

    cancellation

---
+++ src/main/java/com/ams/entity/Cancellation.java
@@ -0,0 +1,40 @@
+package com.ams.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="account_details")
+
+public class Cancellation 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	  private Long ticketid;
+	  @Column
+	  private String source;
+	  @Column
+	  private String destination;
+	  @Column
+	  private Long price;
+	  @Column
+	  private String flightcode;
+	  @Column
+	  private String flightname;
+	  @Column
+	  private Long journeydate;
+	  @Column
+	  private Long journeytime;
+	  @Column
+	  private String username;
+	  @Column
+	  private String name;
+	  @Column
+	  private String reason;
+}
