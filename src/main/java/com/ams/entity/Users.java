4ae57f1a31e7dafd9fde5f370f48e94f587a87db
prangya-1234 <mprangyaparamita956@gmail.com>
2023-07-10 23:31:46 +0530

    Committed Users class

--- src/main/java/com/ams/entity/Users.java
+++ src/main/java/com/ams/entity/Users.java
@@ -1,5 +1,24 @@
 package com.ams.entity;
 
-public class Users {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name="users")
+
+public class Users {
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	private String email;
 }
