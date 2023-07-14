c36f39013f7279ef51b71444cdeceeab794d5014
rajendrarout <rajendrarout888@gmail.com>
2023-07-14 21:52:22 +0530

    flightDetails

---
+++ src/main/java/com/ams/entity/FlightDetails.java
@@ -0,0 +1,28 @@
+package com.ams.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="flight_details")
+
+
+public class FlightDetails {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	@Column
+	private String name;
+	@Column
+	private String flightCode;
+	@Column
+	private String flightZone;
+	
+	
+}
