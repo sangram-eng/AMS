6a9319c1304c97cfed42f4b9b7c2028282452b2e
Diptismita70 <diptismita70@gmail.com>
2023-07-14 22:23:54 +0530

    BookTicket....

---
+++ src/main/java/com/ams/entity/BookTicket.java
@@ -0,0 +1,33 @@
+package com.ams.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="book_ticket")
+
+	public class BookTicket 
+	 {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		  private Long id;
+		  @Column
+		  private PassengerRegistration passengerId;
+		  @Column
+		  private String price;
+		  @Column
+		  private FlightDetails flightId;
+		  @Column
+		  private Date journeydate;
+		  @Column
+		  private String journeytime;
+
+}
